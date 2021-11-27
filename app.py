873172be84f468fceb29f7ad363a9aa8796ecdfc
nowjun <hyunjun1325@icloud.com>
2021-11-28 04:14:30 +0900

    2021.11.28

--- app.py
+++ app.py
@@ -1,81 +1,43 @@
 from flask import Flask, render_template, redirect, url_for, request
+
 import queries as q
-import json
+import time
 
 app = Flask(__name__)
-
 warn = 0
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/bsnum')
-def bsnum():
-    return render_template('bsnum.html')
+@app.route('/slist')
+def slist():
+    return render_template('slist.html')
 
-@app.route('/bsqr', methods=['POST'])
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/bsqr')
 def bsqr():
     result = request.form
-    if not q.is_dup_sno(result['sno']):
-        return render_template('warning.html', message="존재하지 않는 학번입니다.")
     return render_template('bsqr.html', result=result)
-
-@app.route('/beqr', methods=['POST'])
-def beqr():
-    result = request.form
-    if not q.is_code_match(result['sno'], result['bsqr'][:32]):
-        return render_template('warning.html', message="학번과 QR코드가 일치하지 않습니다.")
-    return render_template('beqr.html', result=result)
-
-@app.route('/beimage', methods=['POST'])
-def beimage():
-    result = request.form
-    print(result)
-    if not q.is_dup_eq(result['code']):
-        return render_template('warning.html', message="등록되지 않은 기자재입니다.")
-    return render_template('beimage.html', result=result)
-
-@app.route('/bcal', methods=['POST'])
-def bcal():
-    result = request.form
-    return render_template('bcal.html', result=result)
 
 @app.route('/bdone', methods=['POST'])
 def bdone():
     result = request.form
     print(result)
-    #q.borrow(result['sno'], result['code'], result['image'], result['ddate'])
-    q.borrow(result['sno'], result['code'], None, result['ddate'])
-    return render_template('bdone.html', ddate=result['ddate'], name='아이패드')
 
-@app.route('/pluse')
-def pluse():
-    return render_template('pluse.html')
+    if not q.is_code_match(result['code'][:32]):
+        return render_template('warning.html', message="등록된 QR코드와 일치하지 않습니다.")
+
+    #q.borrow(result['sno'], result['code'], result['image'], result['ddate'])
+    q.borrow(result['code'][:32],)
+    return render_template('bdone.html')
 
 @app.route('/pluss')
 def pluss():
     return render_template('pluss.html')
-
-@app.route('/rdone', methods=['POST'])
-def rdone():
-    result = request.form
-    print(result)
-    q.retum(result['code'])
-    return render_template('rdone.html')
-
-@app.route('/reimage', methods=['POST'])
-def reimage():
-    result = request.form
-    return render_template('reimage.html', result=result)
-
-@app.route('/reqr')
-def reqr():
-    return render_template('reqr.html')
-
-@app.route('/rdon', methods=['POST'])
-def rdon():
-    return redirect(url_for('main'))
 
 @app.route('/bdon', methods=['POST'])
 def bdon():
@@ -83,16 +45,13 @@ def bdon():
 
 @app.route('/psdone', methods=['POST'])
 def psdone():
-    result = request.form
-    q.adduser(result['name'], result['sno'], result['code'][:32])
-    return redirect(url_for('main'))
+    now_time = time.time()
+    tm = time.localtime(now_time)
+    time_string = time.strftime('%Y-%m-%d %H:%M:%S ', tm)
 
-@app.route('/pedone', methods=['POST'])
-def pedone():
     result = request.form
-    q.addeq(result['name'], result['code'])
+    q.adduser(result['name'], result['sno'], result['code'][:32], time_string)
     return redirect(url_for('main'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- queries.py
+++ queries.py
@@ -1,26 +1,40 @@
 import sqlite3
-
+import time
 DATABASE = 'inno.db'
 
-def borrow(sno, code, image, ddate):
-    sql = "INSERT INTO btable (sno, code, image, ddate) VALUES (?, ?, ?, ?)"
-    values = (sno, code, image, ddate)
+def borrow(code):
+    '''sql = "INSERT INTO btable (code) VALUES (?)"
+    values = (code,)'''
+
+    sql = "SELECT sno FROM user WHERE code = (?)"
+    values = (code,)
+    sno = execute_(sql, values)
+    sno = sno[0]
+
+    sql = "SELECT name FROM user WHERE code = (?)"
+    values = (code,)
+    name = execute_(sql, values)
+    name = name[0]
+
+    now_time = time.time()
+    tm = time.localtime(now_time)
+    time_string = time.strftime('%Y-%m-%d %H:%M:%S ', tm)
+
+    sql = "INSERT INTO btable (code, name, sno, time) VALUES (?, ?, ?, ?)"
+    values = (code, name, sno, time_string)
     execute(sql, values)
+
 
 def retum(code):
     sql = "UPDATE btable SET returned = 1 WHERE code = ? AND returned = 0"
     values = (code,)
     execute(sql, values)
 
-def adduser(name, sno, code):
-    sql = "INSERT INTO user (name, sno, code) VALUES (?, ?, ?)"
-    values = (name, sno, code)
+def adduser(name, sno, code, time_string):
+    sql = "INSERT INTO user (name, sno, code, time) VALUES (?, ?, ?, ?)"
+    values = (name, sno, code, time_string)
     execute(sql, values)
 
-def addeq(name, code):
-    sql = "INSERT INTO equipment (name, code) VALUES (?,?)"
-    values = (name, code)
-    execute(sql, values)
 
 def is_dup_sno(sno):
     sql = "SELECT * FROM user WHERE sno = ?"
@@ -34,11 +48,11 @@ def is_dup_eq(code):
 
     return execute_bool(sql, values)
 
-def is_code_match(sno, code):
-    sql = "SELECT * FROM user WHERE sno = ? AND code = ?"
-    values = (sno, code)
-
+def is_code_match(code):
+    sql = "SELECT * FROM user WHERE code = ?"
+    values = (code,)
     return execute_bool(sql, values)
+
 
 def qr_to_eqname(code):
     sql = "SELECT name FROM equipment WHERE cod = ?"
@@ -52,7 +66,7 @@ def execute_(sql, values):
     result = c.fetchone()
     conn.close()
 
-    return result is not None
+    return result
 
 def execute_bool(sql, values):
     conn = sqlite3.connect(DATABASE)
